a8398e4aa6d6dd7766c34fc14b9b7c2c537819ea
NiclasFenton-Wiegleb <niclas.wiegleb@gmail.com>
2023-08-12 10:29:06 +0100

    Investigating trends over time in the dataset.

--- data_handling.py
+++ data_handling.py
@@ -1,6 +1,7 @@
 #Dependencies
 
 import pandas as pd
+import joblib
 
 def load_data():
     #Loads and cleans training data
@@ -38,14 +39,8 @@ def load_data():
 
     return df
 
-def save_model(forecaster, file_path):
-    #Save trained model to directory
-
-    model_filename = f"{file_path}.sav"
-    joblib.dump(forecaster, model_filename)
-
-
-if __name__ == "__main__":
+def load_data_drop_2020():
+    #Loads and cleans training data, dropping most of 2020 outlier data
 
     df = pd.read_csv("train.csv")
 
@@ -71,11 +66,30 @@ if __name__ == "__main__":
         year_week.append("".join([str(year), str(week), "Mon"]))
     
     df["year_week"] = year_week
-
-
     df["year_week"] = pd.to_datetime(df["year_week"], format= "%Y%W%a")
+
+    for loc in df["coordinates"].unique():
+
+        df_loc = df[df["coordinates"]== loc]
+        df_loc = df_loc[df_loc["year"]==2020]
+
+        df.drop(df_loc.index, inplace=True)
+
     df["year_week"] = df["year_week"].dt.to_period("W")
     df.set_index(pd.PeriodIndex(df["year_week"], freq="W"),drop=True, inplace= True)
     df.drop("year_week", axis= 1, inplace=True)
 
-    print(df[df["coordinates"]==(-0.51, 29.29)])
+    return df
+
+def save_model(forecaster, file_path):
+    #Save trained model to directory
+
+    model_filename = f"{file_path}.sav"
+    joblib.dump(forecaster, model_filename)
+
+
+if __name__ == "__main__":
+
+    df = load_data_drop_2020()
+
+    print(df["year"].unique)
